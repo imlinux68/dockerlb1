a2952e5f13b5180e1ca96108e79fc73a851e92c4
im linux <imlinux68@gmail.com>
2023-05-17 12:14:53 +0300

    finished local project

--- app2/app.py
+++ app2/app.py
@@ -1,6 +1,12 @@
 from flask import request, Flask
-import jsonapp1 = Flask(__name__)
-@app1.route('/')def hello_world():
-return 'Hello AWS-RESTART-2023 from app222222!!!! :)'
+import json
+
+
+app2 = Flask(__name__)
+
+
+@app2.route('/')
+def hello_world():
+    return 'Hello AWS-RESTART-2023 from app222222!!!! :)'
 if __name__ == '__main__':
-app1.run(debug=True, host='0.0.0.0')
+    app1.run(debug=True, host='0.0.0.0')
